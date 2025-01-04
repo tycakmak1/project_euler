ea196c88eb4d1af3759e7f5602188de3f69a4220
tycakmak1 <tycakmak1@gmail.com>
2025-01-04 14:48:44 +0100

    problems 5 to 8 are done

--- problem0005/main.cpp
+++ problem0005/main.cpp
@@ -5,3 +5,27 @@
 
 #include <cstdint>
 #include <iostream>
+
+// LCM(x, y) = x * y / GCD(x, y)
+uint32_t gcd(uint32_t a, uint32_t b) {
+    if (a < b) {
+        return gcd(b, a);
+    }
+    // originally it's like this but with big numbers it's inefficient
+    // return b == 0U ? a : gcd(b, a - b);
+    return b == 0U ? a : gcd(b, a - (a / b) * b);
+}
+
+uint64_t lcm(uint64_t a, uint32_t b) { return a * b / gcd(a, b); }
+
+int main() {
+    std::cout << "GCD(19, 20) = " << gcd(19, 20) << '\n';
+    std::cout << "LCM(19, 20) = " << lcm(19, 20) << '\n';
+    uint64_t step{2U};
+    for (uint8_t i = 20U; i >= 3U; i--) {
+        step = lcm(step, i);
+        std::cout << step << '\n';
+    }
+    std::cout << step << '\n';
+    return 0;
+}
