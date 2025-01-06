59883e36073a5875b8d49b0a3b419ec39f85673a
tycakmak1 <tycakmak1@gmail.com>
2025-01-06 21:32:08 +0100

    problem 25 is done

--- problem0011/main.cpp
+++ problem0011/main.cpp
@@ -1,5 +1,5 @@
 // Problem 0011
-// In the 20 x 20 grid below, four numbers along a diagonal line have been marked.
+// In the 20 by 20 grid below, four numbers along a diagonal line have been marked.
 //  8  2 22 97 38 15  0 40  0 75  4  5  7 78 52 12 50 77 91  8
 // 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48  4 56 62  0
 // 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30  3 49 13 36 65
@@ -21,7 +21,7 @@
 // 20 73 35 29 78 31 90  1 74 31 49 71 48 86 81 16 23 57  5 54
 //  1 70 54 71 83 51 54 69 16 92 33 48 61 43 52  1 89 19 67 48
 
-// The product of these numbers is 26 x 63 x 78 x 14 = 1788696.
+// The product of these numbers is 26 * 63 * 78 * 14 = 1788696.
 
 // What is the greatest product of four adjacent numbers in the same direction (up, down, left,
 // right, or diagonally) in this grid?
